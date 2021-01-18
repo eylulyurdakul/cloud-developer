91fb8377979ef8aa2bdb3e48a4d8bac563a425a5
Eylül <eyluldicleyurdakul@gmail.com>
2021-01-18 21:36:44 +0100

    Implement updateTodo

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/updateTodo.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/updateTodo.ts
@@ -3,11 +3,29 @@ import 'source-map-support/register'
 import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
 
 import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
+import {createLogger} from "../../utils/logger";
+import {getUserId} from "../utils";
+import {updateTodoItem} from "../../businessLogic/todos";
+
+const logger = createLogger('updateTodo')
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+const userId = getUserId(event)
   const todoId = event.pathParameters.todoId
   const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
 
+  logger.info('Updating todo:', {userId, todoId, updatedTodo})
+
+  await updateTodoItem(updatedTodo, todoId, userId)
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: ''
+  }
+
   // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
-  return undefined
 }
