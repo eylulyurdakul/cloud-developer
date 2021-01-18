3c06c77b807f66a998400793e3e5c6c5099ecf60
Eylül <eyluldicleyurdakul@gmail.com>
2021-01-18 21:39:15 +0100

    Implement generateUploadUrl

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/generateUploadUrl.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/generateUploadUrl.ts
@@ -1,10 +1,29 @@
 import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import {createLogger} from "../../utils/logger";
+import {createUploadUrl} from "../../businessLogic/todos";
+import {getUserId} from "../utils";
+
+const logger = createLogger('generateUploadUrl')
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const todoId = event.pathParameters.todoId
+  const userId = getUserId(event)
+  const uploadUrl = await createUploadUrl(todoId, userId)
+
+  logger.info('Generating the upload URL: ', uploadUrl)
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify({
+      uploadUrl
+    })
+  }
 
   // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-  return undefined
 }
