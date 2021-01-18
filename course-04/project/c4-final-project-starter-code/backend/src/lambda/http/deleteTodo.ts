0e57ccf6abb0a04b893070449e42d1d92445e5c6
Eylül <eyluldicleyurdakul@gmail.com>
2021-01-18 21:38:01 +0100

    Implement deleteTodo

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/deleteTodo.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/deleteTodo.ts
@@ -1,10 +1,27 @@
 import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import {createLogger} from "../../utils/logger";
+import {getUserId} from "../utils";
+import {deleteTodoItem} from "../../businessLogic/todos";
+
+const logger = createLogger('deleteTodo')
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  const userId = getUserId(event)
   const todoId = event.pathParameters.todoId
 
+  logger.info('Deleteing: ', {userId, todoId})
+
+  await deleteTodoItem(todoId, userId)
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: ''
+  }
   // TODO: Remove a TODO item by id
-  return undefined
 }
