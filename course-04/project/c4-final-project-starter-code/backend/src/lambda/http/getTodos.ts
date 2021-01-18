607302546ab30043c0dd827a42185a6089a3c5a7
Eylül <eyluldicleyurdakul@gmail.com>
2021-01-18 21:34:19 +0100

    Implement getTodos

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/getTodos.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/getTodos.ts
@@ -1,7 +1,27 @@
 import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import {getUserId} from "../utils";
+import {createLogger} from "../../utils/logger";
+import {getAllTodoItemsForUser} from "../../businessLogic/todos";
+
+const logger = createLogger('getTodos')
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   // TODO: Get all TODO items for a current user
+  const userId = getUserId(event)
+
+  logger.info("Get all to-dos for the event:", event)
+
+  const items = await getAllTodoItemsForUser(userId)
+  return {
+      statusCode: 200,
+      headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true
+      },
+      body: JSON.stringify({
+          items
+      })
+  }
 }
